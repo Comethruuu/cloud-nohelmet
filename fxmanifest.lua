470e04a3fc0ceff5f2b37351844bf00ecc6e8c95
Awang Tirta Ananta <90815489+Comethruuu@users.noreply.github.com>
2025-05-24 15:15:39 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,9 +4,8 @@ game 'gta5'
 description 'No Helmet'
 author 'Cloud a.k.a Comethruuu'
 version '1.0.0'
+repository 'https://github.com/Comethruuu/cloud-nohelmet.git'
 
 client_script 'client.lua'
-
 shared_script '@es_extended/imports.lua'
-
 lua54 'yes'
